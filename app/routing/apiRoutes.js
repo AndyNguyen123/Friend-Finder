1553d0994ff699bad8cbfe0ea0a28a6d385b142a
ngbaoan <bambi_19912003@yahoo.com>
2018-07-23 00:01:02 -0700

    cloning app to github

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const players = require('../data/players.js');
 
-router.get('/friends', (req, res) => {
+router.get('/players', (req, res) => {
     res.json(players);
 });
 
