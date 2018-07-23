b4c34499cedaabfc4391aefdb84504053ae854ce
ngbaoan <bambi_19912003@yahoo.com>
2018-07-22 22:25:13 -0700

    using logic to map survey result to type of player

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const players = require('../data/players.js');
 
 router.get('/survey', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+function compareTwoArray (array1, array2) {
+    let resultArray = [];
+    for(let i=0; i<array1.length; i++) {
+        resultArray.push(Math.abs(array1[i] - array2[i]));
+    }
+    return resultArray.reduce((a,b) => a+b);
+}
+
+router.post('/survey', (req, res) => {
+    const answer = req.body["data[]"].map(ele => Number(ele));
+    const playerObjArray = [];
+    let index = 0;
+    players.map(player => player.scores).forEach(scoreArray => {
+        playerObjArray.push({
+            playerIndex: index,
+            score: compareTwoArray(scoreArray, answer)
+        });
+        index++;
+    })
+    Math.min(...playerObjArray.map(ele => ele.score))
+    res.send('respond from server');
 });
 
 router.get('/', (req,res) => {
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require('./app/routing/htmlRoutes.js');
+const htmlRoutes = require('./app/routing/htmlRoutes.js');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(router);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(htmlRoutes);
 
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}...`))
